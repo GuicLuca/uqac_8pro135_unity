8b288b1b04616f563bf9e24852b7078127fa6d25
Marc Dieudonne <mdieudonne@etu.uqac.ca>
2022-10-03 17:15:44 -0400

    FEAT-moovement of the sphere

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,21 +7,30 @@ public class PlayerController : MonoBehaviour
 {
     private InputActionAsset inputAsset;
     private InputActionMap player;
-    public InputAction moveHorizontal;
-    public InputAction moveVertical;
+    private InputAction moveHorizontal;
+    private InputAction moveVertical;
+
+    private Rigidbody rb;
+
+    public float jumpForce = 1;
+    public float speed = 3 ; 
+
 
     void Awake()
     {
         inputAsset = GetComponent<PlayerInput>().actions;
         player = inputAsset.FindActionMap("Player");
+
+        rb = GetComponent<Rigidbody>();
     }
     
     private void OnEnable()
     {
         player.FindAction("Jump").performed += Jump;
-        moveHorizontal = player.FindAction("MoveHorizontal");
-        moveVertical = player.FindAction("MoveVertical");
+        moveHorizontal = player.FindAction("moveHorizontal");
+        moveVertical = player.FindAction("moveVertical");
         player.Enable();
+        
     }
 
     private void OnDisable()
@@ -39,13 +48,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(moveHorizontal.ReadValue<float>());
-        Debug.Log(moveVertical.ReadValue<float>());
-        Debug.Log("\n");
+        Move(moveHorizontal, moveVertical);
     }
 
     void Jump(InputAction.CallbackContext obj)
     {
-        Debug.Log("Jump!");
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void Move(InputAction moveHorizontal, InputAction moveVertical)
+    {
+        Debug.Log(moveVertical.ReadValue<float>());
+        Debug.Log(moveHorizontal.ReadValue<float>());
+        rb.AddForce(new Vector3(moveHorizontal.ReadValue<float>(), 0, moveVertical.ReadValue<float>()) * speed, ForceMode.Force);
     }
 }
