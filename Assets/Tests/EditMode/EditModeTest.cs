33f9fc8a02552f4748b6e87df67b4cca04a9ec18
MattGuil <guillemin.matt@gmail.com>
2022-10-15 17:49:51 -0400

    define 8 tests in Edit Mode and Play Mode

---
+++ Assets/Tests/EditMode/EditModeTest.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class EditModeTest
+{
+    [Test]
+    public void FromSceneEndToMenu()
+    {
+        
+    }
+    
+    [Test]
+    public void ButtonPlayToLevel1()
+    {
+        
+    }
+    
+    [Test]
+    public void QuitGame()
+    {
+        
+    }
+    
+    [Test]
+    public void ChangeMasterVolume()
+    {
+        
+    }
+}
---
+++ Assets/Tests/PlayMode/PlayModeTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PlayModeTest
+{
+    [Test]
+    public void BallFall()
+    {
+        
+    }
+    
+    [Test]
+    public void BallBounceThreeTimes()
+    {
+        
+    }
+    
+    [Test]
+    public void PlaySoundWhenBounce()
+    {
+        
+    }
+
+    [Test]
+    public void PlayerMovement()
+    {
+
+    }
+}
