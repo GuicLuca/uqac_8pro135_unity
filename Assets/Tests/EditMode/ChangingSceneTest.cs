571104071f6aa8d9218bc75977245f792e50fe44
Lucas Guichard <lucasguichard127@gmail.com>
2022-10-15 17:17:48 -0400

    Test - Implementing the architecture for unit test

---
+++ Assets/Tests/EditMode/ChangingSceneTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ChangingSceneTest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void FromSceneEndToMenu()
+    {
+        
+    }
+}
