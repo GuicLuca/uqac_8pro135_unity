d72b7734757a12a43683da39e99706120581a5e4
MattGuil <guillemin.matt@gmail.com>
2022-11-10 11:58:20 -0500

    Feat - Create Object Spawner Window

--- Assets/ObjectSpawnerWindow.cs
+++ Assets/ObjectSpawnerWindow.cs
@@ -3,24 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ObjectSpawnerWindow : EditorWindow
 {
     private GameObject prefabToSpawn;
     private GameObject parentInScene;
-    private string newObjectNamePrefix;
-    private int spawnCount;
+    private string newObjectNamePrefix = "spawned_";
+    private int spawnCount = 5;
     private int spawnedCount = 0;
-    private int maxSpawnDistance;
-    private float minScaleFactor;
-    private float maxScaleFactor;
+    private int maxSpawnDistance = 40;
+    private float minScaleFactor = 0.5f;
+    private float maxScaleFactor = 10.0f;
     
     // Add menu item named "Object Spawner Window" to the Window menu
     [MenuItem ("Window/Object Spawner Window")]
     public static void  ShowWindow () {
         EditorWindow.GetWindow(typeof(ObjectSpawnerWindow));
     }
-    
+
     void OnGUI () {
         GUILayout.Label ("What to spawn", EditorStyles.boldLabel);
         prefabToSpawn = EditorGUILayout.ObjectField("Prefab to spawn", prefabToSpawn, typeof(GameObject), false) as GameObject;
@@ -32,13 +33,30 @@ public class ObjectSpawnerWindow : EditorWindow
         maxSpawnDistance = EditorGUILayout.IntField("Maximum spawn distance", maxSpawnDistance);
         minScaleFactor = EditorGUILayout.FloatField("Minimum scale factor", minScaleFactor);
         maxScaleFactor = EditorGUILayout.FloatField("Maximum scale factor", maxScaleFactor);
+        EditorGUILayout.MinMaxSlider(ref minScaleFactor, ref maxScaleFactor, 0, 20);
 
         if(GUILayout.Button("Spawn " + spawnCount + " objects"))
         {
+            for (int i = spawnedCount; i < spawnedCount + spawnCount; i++)
+            {
+                // Get prefab
+                GameObject prefab = prefabToSpawn;
+                // Set random position
+                Vector3 position = new Vector3(Random.Range(-maxSpawnDistance, maxSpawnDistance), Random.Range(-maxSpawnDistance, maxSpawnDistance), Random.Range(-maxSpawnDistance, maxSpawnDistance));
+                // Set random scale
+                float scaleFactor = Random.Range(minScaleFactor, maxScaleFactor);
+                prefab.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+                // Instantiate the prefab with all this parameters
+                GameObject instantiatedPrefab = Instantiate(prefab, position, Quaternion.identity, parentInScene.transform);
+                // Rename instantiated prefab
+                instantiatedPrefab.name = newObjectNamePrefix + prefab.name + "_" + i;
+            }
+
             spawnedCount += spawnCount;
+            Debug.Log("Spawned " + spawnCount + " objects for a total of " + spawnedCount + " spawned objects!");
         }
 
-        GUILayout.TextArea("Spawned count: " + spawnedCount);
+        EditorGUILayout.LabelField("Spawned count:  " + spawnedCount);
         
         if(GUILayout.Button("Reset spawned count"))
         {
